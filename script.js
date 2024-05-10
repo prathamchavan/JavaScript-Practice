afad13f92425a811ff1f72bc34f67fe1eaf7af97
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-05-10 19:54:18 +0530

    Values and variables

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN");
+console.log(40 + 8 + 23 - 10)
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "MAtilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
