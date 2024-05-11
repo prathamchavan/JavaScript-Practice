e96c3ddf10fd6fd8aa044965d9cc92d21ddde811
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-05-11 18:29:47 +0530

    Data types

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10)
+//let js = "amazing";
+//console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
+/*console.log("Jonas");
 console.log(23);
 
-let firstName = "MAtilda";
+let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
@@ -15,12 +15,33 @@ let $function = 27;
 let person = "Jonas";
 let PI = 3.1415;
 
-let myFirstJob = 'Programmer';
-let myCurrentJob = 'Teacher';
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
 
-let job1 = 'programmer';
-let job2 = 'teacher';
+let job1 = "programmer";
+let job2 = "teacher";
 
 console.log(myFirstJob);
 console.log(myCurrentJob);
+*/
 
+let javascriptIsFun = false;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 60);
+console.log(typeof "Prathamesh");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1999;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
