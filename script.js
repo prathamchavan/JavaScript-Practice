01546f9714e5198009db6b01d31e2cc079e2e65e
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-05-17 19:10:14 +0530

    BMI challenge one

--- script.js
+++ script.js
@@ -134,4 +134,16 @@ Copy to Clipboard
 // └──────│───── 3. ─────│──────┘
 //        └───── 4. ─────┘
 
+
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
 */
