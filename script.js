ce2db2f96b9fe71b9fcb21bd1ad4adb7c3be1297
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-05-20 00:45:35 +0530

    function declaration vs expression

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ if (hasDriversLicense) console.log("I can drive :D");
 */
 
 //function
-
+/*
 function logger() {
   console.log("My name is Jonas");
 }
@@ -32,3 +32,20 @@ console.log(fruitProcessor(5, 0));
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+*/
+
+//function declaration
+function calcAge1(birthYeah) {
+  return 2037 - birthYeah;
+}
+
+const age1 = calcAge1(1991);
+
+//function expression
+const calcAge2 = function (birthYeah) {
+  return 2037 - birthYeah;
+};
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
