631f89577cd766798f785c2f87601b2817e8ef60
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-05-22 19:55:56 +0530

    Basic array operation (methods)

--- script.js
+++ script.js
@@ -152,6 +152,7 @@ function checkWinner() {
 console.log(checkWinner());
 */
 
+/*
 //Introduction to arrays
 
 const friends = ["Michael", "Steven", "Peter"];
@@ -192,3 +193,35 @@ const ages = [
   calcAge(years[0], calcAge(years[1]), calcAge(years[years.length - 1])),
 ];
 console.log(ages);
+ */
+
+//Add elements
+const friends = ["Michael", "Steven", "Peter"];
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+//Remove elements
+friends.pop();
+const popped = friends.pop(); //last
+console.log(popped);
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+//this method checks weather element is in array or not
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Steven");
+}
