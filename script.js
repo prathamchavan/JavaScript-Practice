4e10c11b9cba4c4543396345ed6d2574234ccf65
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-05-27 18:19:03 +0530

    coding challenge object

--- script.js
+++ script.js
@@ -298,13 +298,63 @@ console.log(bills, tips, total);
 
 //////////////////// Object Constructor Functions ////////////////////
 
-function person(first, last, age, eye) {
-  this.firstName = first;
-  this.lastName = last;
-  this.age = age;
-  this.eyeColor = eye;
-}
+// function person(first, last, age, eye) {
+//   this.firstName = first;
+//   this.lastName = last;
+//   this.age = age;
+//   this.eyeColor = eye;
+// }
 
-const myFather = new person("Pravin", "Chavan", 52, "Brown");
-console.log(myFather["firstName"]);
-console.log(myFather.firstName);
+// const myFather = new person("Pravin", "Chavan", 52, "Brown");
+// console.log(myFather["firstName"]);
+// console.log(myFather.firstName);
+
+/////////// object coding challenge //////////
+
+/* 
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to 
+implement the calculations! Remember: BMI = mass / height ** 2 = mass 
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and 
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same 
+method on both objects). Store the BMI value to a property, and also return it 
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the 
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m 
+tall.
+
+*/
+
+const John = {};
+John.firstName = "John";
+John.lastName = "Smith";
+John.mass = 92;
+John.height = 1.95;
+
+const Mark = {};
+Mark.firstName = "Mark";
+Mark.lastName = "Miller";
+Mark.mass = 78;
+Mark.height = 1.69;
+
+const calcBMI = function (mass, height) {
+  return mass / height ** 2;
+};
+
+const JohnBMI = Math.round(calcBMI(John.mass, John.height));
+const MarkBMI = Math.round(calcBMI(Mark.mass, Mark.height));
+
+console.log(JohnBMI, MarkBMI);
+
+if (MarkBMI > JohnBMI) {
+  console.log(
+    "Mark's BMI (" + MarkBMI + ") is higher than Mark's (" + JohnBMI + ")!"
+  );
+} else {
+  console.log(
+    "Mark's BMI (" + JohnBMI + ") is higher than Mark's (" + MarkBMI + ")!"
+  );
+}
