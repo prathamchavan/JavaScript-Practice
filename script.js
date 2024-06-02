7c057c85e0f82e18d99174b1e9d813995658168b
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-06-02 20:29:40 +0530

    Sum of natural numbers in given range

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 // program for calculating sum of first N natural numbers
 
 const num = Number(prompt("Enter a number "));
@@ -7,3 +8,18 @@ for (let i = 0; i <= num; i++) {
 }
 
 console.log("The sum of first natural numbers until " + num + " is:" + sum);
+
+*/
+
+// Program for sum of the numbers in a given range
+
+const start = parseInt(prompt("Enter the starting number: "));
+const end = parseInt(prompt("Enter the last number: "));
+
+let sum = 0;
+
+for (i = start; i <= end; i++) {
+  sum = sum + i;
+}
+
+console.log(sum);
